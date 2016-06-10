41fa09e48d9b0a038cab6377311bf8d7757b49b6
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-06-10 12:44:28 +0800

    Begin GameVC and creates Card xib file

---
+++ BrainTeaser/Card.swift
@@ -0,0 +1,47 @@
+//
+//  Card.swift
+//  BrainTeaser
+//
+//  Created by Kersuzan on 10/06/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+import UIKit
+
+class Card: UIView {
+
+    let shapes = ["shape1", "shape2", "shape3"]
+    var currentShape: String!
+    
+    @IBOutlet weak var shapeImage: UIImageView!
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+        selectShape()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+
+    func setUpView() {
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSizeMake(0.0, 2.0)
+        self.layer.shadowColor = UIColor(red: 157.0 / 255.0, green: 157.0 / 255.0, blue: 157.0 / 255.0, alpha: 1.0).CGColor
+        self.setNeedsLayout()
+    }
+    
+    func selectShape() {
+        currentShape = shapes[Int(arc4random_uniform(UInt32(shapes.count)))]
+        shapeImage.image = UIImage(named: currentShape)
+    }
+}
---
+++ BrainTeaser/GameVC.swift
@@ -0,0 +1,17 @@
+//
+//  GameVC.swift
+//  BrainTeaser
+//
+//  Created by Kersuzan on 10/06/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
